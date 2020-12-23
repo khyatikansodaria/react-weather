6f576e581b0dd9c3df5462b60fbada64d1dd65e9
khyatipatel1624 <70243342+khyatipatel1624@users.noreply.github.com>
2020-12-22 16:56:27 -0800

    remove props from Prediction

--- src/Prediction.js
+++ src/Prediction.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Prediction.css";
 
-export default function Prediction(props) {
-  console.log(props.currentCity);
+export default function Prediction() {
+  // console.log(props.currentCity);
   return (
     <div className="row row-cols-2 row-cols-sm-2 row-cols-md-5 text-center forecast mt-5">
       <div className="col">
