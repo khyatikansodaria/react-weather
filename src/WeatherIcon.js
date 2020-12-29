5903dcf091684e235642636848fa48ab7aba420b
khyatipatel1624 <70243342+khyatipatel1624@users.noreply.github.com>
2020-12-29 13:42:15 -0800

    changed background

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -25,7 +25,7 @@ export default function WeatherIcon(props){
     return(
         <ReactAnimatedWeather
             icon={codeMapping[props.code]}
-            color="#213338"
+            color="#41444b"
             size={70}
             animate={true}
         />
